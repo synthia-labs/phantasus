623f8c0ffb5c51a45b318cc575e4f8cdc01c4027
Siraj Farhan <44821159+sirajfarhan@users.noreply.github.com>
2024-05-28 09:42:52 +0300

    Update phantasus-package.R

--- R/phantasus-package.R
+++ R/phantasus-package.R
@@ -4,3 +4,15 @@
 #' @name fgseaExample
 NULL
 
+.onLoad <- function(libname, pkgname) {
+  library(withr)
+  withr::with_dir(tempdir(), {
+    Sys.setenv("PHANTASUS_CACHE_ROOT" = "/root/.cache/R/phantasus")
+    Sys.setenv("PHANTASUS_GEO_CACHE" = "/root/.cache/R/phantasus/geo")
+    Sys.setenv("PHANTASUS_ANNOTATIONDB" = "/root/.cache/R/phantasus/annotationdb")
+    Sys.setenv("PHANTASUS_FGSEA_PATHWAYS" = "/root/.cache/R/phantasus/fgsea_pathways")
+    Sys.setenv("PHANTASUS_RNASEQ_COUNTS" = "https://alserglab.wustl.edu/hsds/?domain=/counts")
+
+    setupPhantasus()
+  })
+}
